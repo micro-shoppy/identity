2fbac47281b7954b65ce0e667521c0d408512837
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-07-27 21:27:03 +0200

    Add domain entities with basic unit tests

---
+++ MicroShoppy.Identity/src/core/MicroShoppy.Identity.Domain/Entities/Role.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MicroShoppy.Identity.Domain.Common;
+using MicroShoppy.Identity.Domain.ValueObjects;
+
+namespace MicroShoppy.Identity.Domain.Entities
+{
+    public class Role : AuditableEntity<Guid>
+    {
+        public RoleName RoleName { get; protected set; }
+        public ICollection<User> Users { get; protected set; }
+        protected Role()
+        {
+            Users = new List<User>();
+        }
+        public Role(Guid id, DateTime createdAt, RoleName roleName) : base(id, createdAt)
+        {
+            if (roleName == null)
+            {
+                throw new ArgumentNullException(nameof(roleName), $"{nameof(RoleName)} property in {nameof(Role)} entity must not be null.");
+            }
+
+            RoleName = roleName;
+            Users = new List<User>();
+        }
+
+    }
+}
