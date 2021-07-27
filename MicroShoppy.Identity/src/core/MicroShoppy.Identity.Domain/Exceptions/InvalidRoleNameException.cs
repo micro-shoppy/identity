33a05b9556d81220b616c79aa86f3f53ac2580fd
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-07-27 21:25:47 +0200

    Add Domain exceptions

---
+++ MicroShoppy.Identity/src/core/MicroShoppy.Identity.Domain/Exceptions/InvalidRoleNameException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MicroShoppy.Identity.Domain.Common;
+
+namespace MicroShoppy.Identity.Domain.Exceptions
+{
+    public class InvalidRoleNameException : DomainException
+    {
+        public InvalidRoleNameException(string roleName, Exception ex) : base($"Role name \"{roleName}\" is invalid.", ex)
+        {
+        }
+
+        public InvalidRoleNameException(string roleName) : base($"Role name \"{roleName}\" is invalid.")
+        {
+        }
+    }
+}
---
+++ MicroShoppy.Identity/src/core/MicroShoppy.Identity.Domain/Exceptions/UserNotFoundDomainException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MicroShoppy.Identity.Domain.Common;
+using MicroShoppy.Identity.Domain.ValueObjects;
+
+namespace MicroShoppy.Identity.Domain.Exceptions
+{
+    public class UserNotFoundDomainException : NotFoundDomainException
+    {
+        public UserNotFoundDomainException(UserName userName, Exception ex) : base($"User with name \"{userName.Name}\" not found.", ex)
+        {
+        }
+
+        public UserNotFoundDomainException(UserName userName) : base($"User with name \"{userName.Name}\" not found.")
+        {
+        }
+
+        public UserNotFoundDomainException(Email email) : base($"User with email \"{email.FullEmail}\" not found.")
+        {
+        }
+
+        public UserNotFoundDomainException(Guid id) : base($"User with ID \"{id}\" not found.")
+        {
+        }
+    }
+}
