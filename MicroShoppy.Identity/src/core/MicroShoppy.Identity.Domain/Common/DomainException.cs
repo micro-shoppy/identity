f1a302eb3c2d553a8462414d4c2d9ec18127fd91
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-07-27 21:22:05 +0200

    Add common classes

---
+++ MicroShoppy.Identity/src/core/MicroShoppy.Identity.Domain/Common/DomainException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroShoppy.Identity.Domain.Common
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+
+        public DomainException(string message, Exception ex) : base(message, ex)
+        {
+        }
+    }
+}
