a570aa1d70f6d7ea0eb82297c8bd886aa8090d02
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-07-27 21:22:05 +0200

    Add common classes

---
+++ MicroShoppy.Identity/src/core/MicroShoppy.Identity.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroShoppy.Identity.Domain.Common
+{
+    public class AuditableEntity<T>
+    {
+        public T Id { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+
+        protected AuditableEntity()
+        {
+
+        }
+        public AuditableEntity(T id, DateTime createdAt)
+        {
+            Id = id;
+            CreatedAt = createdAt;
+        }
+    }
+}
---
+++ MicroShoppy.Identity/src/core/MicroShoppy.Identity.Domain/Common/NotFoundDomainException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroShoppy.Identity.Domain.Common
+{
+    public class NotFoundDomainException : DomainException
+    {
+        public NotFoundDomainException(string message) : base(message)
+        {
+        }
+
+        public NotFoundDomainException(string message, Exception ex) : base(message, ex)
+        {
+        }
+    }
+}
