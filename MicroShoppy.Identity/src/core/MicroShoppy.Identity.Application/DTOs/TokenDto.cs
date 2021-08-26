0071c8beec3c4b969d53322f9e7880d981b8865a
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-08-26 21:01:56 +0200

    Add DTOs

---
+++ MicroShoppy.Identity/src/core/MicroShoppy.Identity.Application/DTOs/TokenDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MicroShoppy.Identity.Application.DTOs
+{
+    public class TokenDto
+    {
+        public string Token { get; set; }
+        public string Expires { get; set; }
+    }
+}
---
+++ MicroShoppy.Identity/src/core/MicroShoppy.Identity.Application/DTOs/UserProfileDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MicroShoppy.Identity.Application.DTOs
+{
+    public class UserProfileDto
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
