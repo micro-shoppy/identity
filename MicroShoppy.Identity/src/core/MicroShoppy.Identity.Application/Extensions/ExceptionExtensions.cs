554a0287c72bba00749bcfa03d3aed21ee514346
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-08-26 21:01:27 +0200

    Add new exceptions and its extensions

---
+++ MicroShoppy.Identity/src/core/MicroShoppy.Identity.Application/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MicroShoppy.Identity.Domain.Common;
+
+namespace MicroShoppy.Identity.Application.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static bool IsDomainException(this Exception ex)
+        {
+            return ex is DomainException;
+        }
+
+        public static bool IsNotFoundDomainException(this Exception ex)
+        {
+            return ex is NotFoundDomainException;
+        }
+    }
+}
---
+++ MicroShoppy.Identity/src/core/MicroShoppy.Identity.Domain/Exceptions/UserAlreadyExistsDomainException.cs
@@ -0,0 +1,11 @@
+﻿using MicroShoppy.Identity.Domain.Common;
+
+namespace MicroShoppy.Identity.Domain.Exceptions
+{
+    public class UserAlreadyExistsDomainException : DomainException
+    {
+        public UserAlreadyExistsDomainException() : base("User with this combination of user name and email already exists.")
+        {
+        }
+    }
+}
