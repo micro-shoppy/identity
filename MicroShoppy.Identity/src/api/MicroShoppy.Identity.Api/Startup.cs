8a5bbf222576d2e53d834635803b294e79a9a56e
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-08-26 21:03:35 +0200

    Register everything in IoC container

--- MicroShoppy.Identity/src/api/MicroShoppy.Identity.Api/Startup.cs
+++ MicroShoppy.Identity/src/api/MicroShoppy.Identity.Api/Startup.cs
@@ -9,8 +9,22 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using MediatR;
+using MicroShoppy.Identity.Api.Middleware;
+using MicroShoppy.Identity.Application.Helpers;
+using MicroShoppy.Identity.Application.Repositories;
+using MicroShoppy.Identity.Application.Services;
+using MicroShoppy.Identity.Application.Settings;
+using MicroShoppy.Identity.Application.Users.Commands;
+using MicroShoppy.Identity.Domain.ValueObjects;
+using MicroShoppy.Identity.Infrastructure.Persistence;
+using MicroShoppy.Identity.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace MicroShoppy.Identity.Api
 {
@@ -28,10 +42,72 @@ namespace MicroShoppy.Identity.Api
         {
 
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MicroShoppy.Identity.Api", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        Name = "Bearer",
+                        BearerFormat = "JWT",
+                        Scheme = "Bearer",
+                        Description = "Authorization token",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.Http,
+                    });
+
+                c.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Id = "Bearer",
+                                    Type = ReferenceType.SecurityScheme
+                                }
+                            },
+                            Array.Empty<string>()
+                        }
+                    });
             });
+
+            services.AddSingleton<IUserRepository, UserRepository>();
+            services.AddTransient<IJwtHandler, JwtHandler>();
+            services.AddMediatR(typeof(CreateUserCommand).Assembly);
+
+            UserPassword.VerifyPasswordFunction = PasswordHasher.VerifyPassword;
+            UserPassword.HashFunction = PasswordHasher.CalculateHash;
+
+            var authSection = Configuration.GetSection("AuthOptions");
+            var authOptions = authSection.Get<AuthOptions>();
+            services.Configure<AuthOptions>(authSection);
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(jwt =>
+                {
+                    var key = Encoding.ASCII.GetBytes(authOptions.Key);
+
+                    jwt.SaveToken = true;
+                    jwt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        RequireExpirationTime = true,
+                        ValidateLifetime = true
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,9 +120,13 @@ namespace MicroShoppy.Identity.Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MicroShoppy.Identity.Api v1"));
             }
 
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
