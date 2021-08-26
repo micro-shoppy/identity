4912a5b022211a0b6ac355b11c84c9bb9f40c15f
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-08-26 21:03:05 +0200

    Add Users controller with CQRS cases

---
+++ MicroShoppy.Identity/src/core/MicroShoppy.Identity.Application/Users/Commands/CreateUserCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using MicroShoppy.Identity.Application.Repositories;
+using MicroShoppy.Identity.Domain.Entities;
+using MicroShoppy.Identity.Domain.Exceptions;
+using MicroShoppy.Identity.Domain.ValueObjects;
+
+namespace MicroShoppy.Identity.Application.Users.Commands
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Guid>
+    {
+        private readonly IUserRepository _repository;
+
+        public CreateUserCommandHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Guid> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            if (await _repository.UserExists(UserName.For(request.UserName), Email.For(request.Email)))
+            {
+                throw new UserAlreadyExistsDomainException();
+            }
+
+            var user = new User(
+                Guid.NewGuid(),
+                DateTime.UtcNow,
+                Email.For(request.Email),
+                UserName.For(request.UserName),
+                UserPassword.For(request.Password),
+                await _repository.GetDefaultRoleAsync());
+
+            await _repository.AddAsync(user);
+
+            return user.Id;
+        }
+    }
+}
---
+++ MicroShoppy.Identity/src/core/MicroShoppy.Identity.Application/Users/Queries/GetUserQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using MicroShoppy.Identity.Application.DTOs;
+using MicroShoppy.Identity.Application.Repositories;
+using MicroShoppy.Identity.Domain.Entities;
+
+namespace MicroShoppy.Identity.Application.Users.Queries
+{
+    public class GetUserQueryHandler : IRequestHandler<GetUserQuery, UserProfileDto>
+    {
+        private readonly IUserRepository _repository;
+
+        public GetUserQueryHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<UserProfileDto> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _repository.GetAsync(request.Id);
+
+            return new UserProfileDto
+            {
+                Email = user.Email.FullEmail,
+                UserName = user.UserName.Name,
+                Roles = user.Roles.Select(x => x.RoleName.Name)
+            };
+        }
+    }
+}
