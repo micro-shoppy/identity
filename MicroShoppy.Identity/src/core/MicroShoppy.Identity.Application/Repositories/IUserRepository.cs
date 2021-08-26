e32f653404f1e4876fce71a5f932ad973221c228
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-08-26 21:02:29 +0200

    Add inMemory User repository

---
+++ MicroShoppy.Identity/src/core/MicroShoppy.Identity.Application/Repositories/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MicroShoppy.Identity.Domain.Entities;
+using MicroShoppy.Identity.Domain.ValueObjects;
+
+namespace MicroShoppy.Identity.Application.Repositories
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(User user);
+        Task<Role> GetDefaultRoleAsync();
+        Task<bool> UserExists(UserName userName, Email email);
+        Task<bool> UserExists(UserName userName);
+        Task<User> GetAsync(Guid id);
+        Task<User> GetAsync(UserName userName);
+        Task<User> GetAsync(Email email);
+    }
+}
